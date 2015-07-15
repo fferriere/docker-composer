02bad2c24d3384cff8730a6c0a44d81510dfddc9
FERRIERE Florian <ferriere.florian@gmail.com>
2015-07-15 14:42:41 +0200

    Add rebuild composer-base option

--- composer-base/build.sh
+++ composer-base/build.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 IMAGE_NAME="fferriere/composer-base"
+FORCE_REBUILD=0
+if [ "$1" == "rebuild" ]; then
+    FORCE_REBUILD=1
+    shift
+fi
+
 NB_ROWS=$(docker images | grep -w "$IMAGE_NAME" | wc -l)
-if [ "$NB_ROWS" -gt 0 ]; then
+if [ "$NB_ROWS" -gt 0 ] && [ $FORCE_REBUILD -eq 0 ]; then
     exit 0;
 fi
 
 MY_PATH=$(dirname $(realpath $0))
-docker build -t $IMAGE_NAME $MY_PATH/.
+docker build -t $IMAGE_NAME $@ $MY_PATH/.
+
+docker tag $IMAGE_NAME $IMAGE_NAME:init
